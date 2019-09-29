696b258f70ab8021eb84a19a1629968171876b03
prairir <me@ryanprairie.com>
2019-09-28 21:04:21 -0400

    added a server file system setup script

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, abort
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/form')
+def about():
+    return render_template('form.html')
+
+@app.route('/get/<rfile>')
+def images(rfile):
+    try:
+        return send_from_directory("./static/",rfile)
+    except:
+        return abort(404)
 
 if __name__ == '__main__':
     app.run(debug = True)
